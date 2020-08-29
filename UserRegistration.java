622c4c180f67f42bf88342d9574338f1b760664f
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-29 12:23:40 +0530

    Added:Functionality to validate mobile number

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     static String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
     static  String EMAIL_PATTERN = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
+    static String MOBILE_NUMBER_PATTERN="^[0-9]{1,3}[' '][0-9]{10}$";
 
     public static boolean validInputName(String inputName) {
         return inputName.matches(NAME_PATTERN);
@@ -12,8 +13,11 @@ public class UserRegistration {
         return emailId.matches(EMAIL_PATTERN);
     }
 
+    public static boolean validateMobileNumber(String mobileNumber) {
+        return mobileNumber.matches(MOBILE_NUMBER_PATTERN);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
     }
-
 }
