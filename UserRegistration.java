16f541f7ea42a642cd7d44a1e27de01cac25a010
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-29 12:08:27 +0530

    Added:Functionality to validate email

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,9 +2,14 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     static String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+    static  String EMAIL_PATTERN = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
 
     public static boolean validInputName(String inputName) {
         return inputName.matches(NAME_PATTERN);
+    }
+
+    public static boolean validateEmailId(String emailId) {
+        return emailId.matches(EMAIL_PATTERN);
     }
 
     public static void main(String[] args) {
